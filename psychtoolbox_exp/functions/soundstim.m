7683f707aa223775dfa3cbff731de645172328a7
Jorie van Haren <105725985+jorievanharen@users.noreply.github.com>
2025-11-11 16:29:31 +0100

    Delete psychtoolbox_exp directory

--- psychtoolbox_exp/functions/soundstim.m
+++
@@ -1,6 +0,0 @@
-function xt = soundstim(freq, stim_len, samp_rate)
-% Generate sound stimulus for some length
-
-xt = sin(2*pi*freq*(0:stim_len*samp_rate-1)/samp_rate);
-
-return;
--- psychtoolbox_exp/functions/waitfornokey.m
+++
@@ -1,7 +0,0 @@
-function [] = waitfornokey
-while 1
-    [a,~,~] = KbCheck;
-    if a ==0;
-        break
-    end
-end
